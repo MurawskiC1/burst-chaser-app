6a45b95cedb5df7c73b2dda9282368fb2769be6f
MurawskiC1 <carter.murawski@spartans.ut.edu>
2024-05-17 14:36:32 -0500

    add and get functions in back end

--- client/src/functions/Exports.js
+++ client/src/functions/Exports.js
@@ -1,13 +1,15 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
+const table = "new_table";
+
 export function useBursts() {
     const [out, setOut] = useState([]);
 
     useEffect(() => {
         const fetchAllBursts = async () => {
             try {
-                const res = await axios.get("http://localhost:8800/new_table");
+                const res = await axios.get(`http://localhost:8800/${table}`);
                 setOut(res.data);
             } catch (err) {
                 console.error(err);
@@ -19,6 +21,23 @@ export function useBursts() {
     return out;
 }
 
+export function useAddBurst(name, desc, number) {
+    const addBurst = async () => {
+        const burst = {
+            name: name,
+            description: desc,
+            number: number
+        };
+
+        try {
+            const res = await axios.post(`http://localhost:8800/${table}`, burst);
+            // Handle the result here if needed
+        } catch (err) {
+            console.error(err);
+        }
+    };
+    addBurst()
+}
 
 
 
--- client/src/pages/Data.jsx
+++ client/src/pages/Data.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
-import { useBursts } from '../functions/Exports';
+import { useBursts, useAddBurst } from '../functions/Exports';
 
 export default function Data(props) {
     const bursts = useBursts();
+
+    // Define a function to handle adding a burst
+    const handleAddBurst = () => {
+        useAddBurst("Campbell", "IS", 57); // Call useAddBurst when the button is clicked
+    };
 
     return (
         <div>
             {bursts.map((burst, index) => (
                 <div key={index}>
+                    {burst.name}
+                    {burst.description}
                     {burst.number}
                 </div>
             ))}
+            {/* Button to add a burst */}
+            <button onClick={handleAddBurst}>Add</button>
         </div>
     );
 }
+
 
 
