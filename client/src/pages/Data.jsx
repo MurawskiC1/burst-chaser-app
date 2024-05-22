420588cfe36f1b1f5096efd2d31d12878446e0b9
MurawskiC1 <carter.murawski@spartans.ut.edu>
2024-05-22 14:09:41 -0500

    Page numbers

--- client/src/pages/Data.jsx
+++ client/src/pages/Data.jsx
@@ -4,15 +4,25 @@ import { useBursts } from '../functions/Exports';
 
 export default function Data(props) {
     const [filter, setFilter] = useState('');
-    const [limit, setLimit] = useState(50);
+    const [start, setStart] = useState(0);
+    const [end, setEnd] = useState(50);
     const [searchQuery, setSearchQuery] = useState('');
 
     const handleTypeChange = (newType) => {
-        setFilter(`verify LIKE '${newType}%'`);
-    };
+        setFilter(currentFilter => {
+            console.log(currentFilter)
+            if (newType == null) {
+                return 'verify = ""'
+            }
+            if (currentFilter == "" || currentFilter.includes("AND") || currentFilter.includes("=")) {
+                return `verify LIKE '%${newType}%'`
+            } else {
+                return currentFilter + ` AND verify LIKE '%${newType}%'`
+            }
+        })
+    }
 
-
-    const bursts = useBursts("pulse_shape", filter, limit);
+    const bursts = useBursts("pulse_shape", filter);
 
     const handleSearchChange = (e) => {
         setSearchQuery(e.target.value);
@@ -22,6 +32,26 @@ export default function Data(props) {
         burst.Burst_Name.toLowerCase().includes(searchQuery.toLowerCase()) ||
         burst.BurstID.toLowerCase().includes(searchQuery.toLowerCase())
     );
+
+    const handleChangePage = (direction) => {
+        const change = end - start
+        if (direction == "next" && start + change <= filteredBursts.length) {
+            setStart(page => {
+                return page + change
+            })
+            setEnd(page => {
+                return page + change
+            })
+        }
+        if (direction == "previous" && start - change >= 0) {
+            setStart(page => {
+                return page - change
+            })
+            setEnd(page => {
+                return page - change
+            })
+        }
+    }
 
     return (
         <div>
@@ -38,13 +68,14 @@ export default function Data(props) {
                     <button onClick={() => handleTypeChange('Extended')}>Extended</button>
                     <button onClick={() => handleTypeChange('Other')}>Other</button>
                     <button onClick={() => handleTypeChange('Too Noisy')}>Too Noisy</button>
-                    <button onClick={() => handleTypeChange('None')}>Not Classified</button>
+                    <button onClick={() => handleTypeChange(null)}>Not Classified</button>
                 </div>
             </div>
             <div className='data-container'>
                 <table>
                     <thead>
                         <tr>
+                            <th>Index</th>
                             <th>Image</th>
                             <th>Burst Name</th>
                             <th>Burst ID</th>
@@ -56,8 +87,9 @@ export default function Data(props) {
                         </tr>
                     </thead>
                     <tbody>
-                        {filteredBursts.map((burst) => (
+                        {filteredBursts.slice(start, end).map((burst, index) => (
                             <tr key={burst.BurstID}>
+                                <td>{start + index + 1}</td>
                                 <td><img src={`assets/BurstPhotos/${burst.Burst_PNG}`} alt={burst.Burst_Name} /></td>
                                 <td><Link to={`${burst.Burst_Name}`}>{burst.Burst_Name}</Link></td>
                                 <td>{burst.BurstID}</td>
@@ -69,10 +101,19 @@ export default function Data(props) {
                             </tr>
                         ))}
                     </tbody>
+
                 </table>
+
             </div>
+
+            <div className="flippage-containter">
+                <button onClick={() => handleChangePage("next")}>Next</button>
+                <div>Page Number {Math.floor(end / (end - start))} of {Math.floor(filteredBursts.length / (end - start)) + 1} </div>
+                <button onClick={() => handleChangePage("previous")}>Previous</button>
+            </div>
+
         </div>
-    );
-}
+    )
+};
 
 
